e4252097136dbf2f82ffeb767457bfcd59541e0b
moein <mohammadmoein.moradi@gmail.com>
2021-11-15 20:16:30 +0330

    refactor Solver class

--- solver.cpp
+++ solver.cpp
@@ -1,26 +1,29 @@
 class Solver {
     public:
         Solver() {}
-        int solve() {
-            std::cout << "Running solver" << std::endl;
-            readBoard();
-            
-            Validator v(board);
-            validator = &v;
-            
-            return 0;
-        }
-    
+        int solve(); 
     private:
+        void readBoard();
         std::string board[9];
         Validator *validator;
-        void readBoard() {
-            std::ifstream boardfile("board.txt");
-            for (int i=0; i<9; i++){
-                getline(boardfile, board[i]);
-                std::cout<< "line " << i << " " << board[i] << std::endl;
-            }
-            boardfile.close();
-        }
 };
+
+int Solver::solve() {
+    std::cout << "Running solver" << std::endl;
+    readBoard();
+    
+    Validator v(board);
+    validator = &v;
+    
+    return 0;
+}
+
+void Solver::readBoard() {
+    std::ifstream boardfile("board.txt");
+    for (int i=0; i<9; i++){
+        getline(boardfile, board[i]);
+        std::cout<< "line " << i << " " << board[i] << std::endl;
+    }
+    boardfile.close();
+}
 
