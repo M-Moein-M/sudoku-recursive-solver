68ddeae7c57e78a69898483baf12e5ebc4ace177
moein <mohammadmoein.moradi@gmail.com>
2021-11-28 17:27:21 +0330

    add checkGrids method to Validator

--- validator.cpp
+++ validator.cpp
@@ -8,13 +8,15 @@ class Validator {
         std::string *board;
         bool useNumber(char *, int);
         bool checkRowOrCol(bool testRows);
+        bool checkGrids();
         char NoSegFault;  // removing this results in segfault ???
 };
 
 bool Validator::isValid() {
     bool validCols = checkRowOrCol(false);
     bool validRows = checkRowOrCol(true);
-    return validCols && validRows;
+    bool validGrids = checkGrids();
+    return validCols && validRows && validGrids;
 }
 
 bool Validator::useNumber(char *used, int boardVal) {
@@ -24,6 +26,19 @@ bool Validator::useNumber(char *used, int boardVal) {
     else if (used[numVal] >= 1)
         return false;
     used[numVal] += 1;
+    return true;
+}
+
+bool Validator::checkGrids() {
+    for (int k=0; k<9; k++) {
+        char *used = (char*)calloc(10, sizeof(char));
+        for (int i=0; i<3; i++) 
+            for (int j=0; j<3; j++) {
+                bool usedOnce = useNumber(used, board[k/3*3+i][k%3*3+j]);
+                if (!usedOnce)
+                    return false;
+            }
+    }
     return true;
 }
 
